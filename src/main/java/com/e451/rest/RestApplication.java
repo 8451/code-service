e3f0a87b3fb1d3f1c558f94980088dc49cd8af3e
Owen Wells <owen.wells@8451.com>
2017-06-05 13:41:40 -0400

    Initial Commit

---
+++ src/main/java/com/e451/rest/RestApplication.java
@@ -0,0 +1,12 @@
+package com.e451.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/e451/rest/domains/DomainPojoTest.java
@@ -0,0 +1,47 @@
+package com.e451.rest.domains;
+
+import com.openpojo.reflection.PojoClass;
+import com.openpojo.reflection.PojoClassFilter;
+import com.openpojo.reflection.impl.PojoClassFactory;
+import com.openpojo.validation.Validator;
+import com.openpojo.validation.ValidatorBuilder;
+import com.openpojo.validation.rule.impl.GetterMustExistRule;
+import com.openpojo.validation.rule.impl.NoPublicFieldsExceptStaticFinalRule;
+import com.openpojo.validation.rule.impl.SetterMustExistRule;
+import com.openpojo.validation.test.impl.GetterTester;
+import com.openpojo.validation.test.impl.SetterTester;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class DomainPojoTest {
+    private List<PojoClass> pojoClasses;
+    private final PojoClassFilter filterTestClasses = new FilterTestClasses();
+
+    @Before
+    public void setup(){
+        String packageName = "com.e451.rest.domains"; // only scan the DTOs
+        pojoClasses = PojoClassFactory.getPojoClassesRecursively(packageName, filterTestClasses);
+    }
+
+    @Test
+    public void validate() {
+        Validator validator = ValidatorBuilder.create()
+                .with(new SetterMustExistRule())
+                .with(new GetterMustExistRule())
+                .with(new SetterTester())
+                .with(new GetterTester())
+                .with(new NoPublicFieldsExceptStaticFinalRule())
+                .build();
+
+        validator.validate(pojoClasses);
+    }
+
+    // Filter out test classes copied to target/test-classes by Maven
+    private static class FilterTestClasses implements PojoClassFilter {
+        public boolean include(PojoClass pojoClass) {
+            return !pojoClass.getSourcePath().contains("test-classes");
+        }
+    }
+}
