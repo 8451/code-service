0a4a8b1b390a38a84626015ba6d32c8b91f58a25
Jake Zarobsky <Jake.Zarobsky@8451.com>
2017-07-27 16:26:17 -0400

    Fix the token util

--- src/main/java/com/e451/rest/security/JwtTokenUtil.java
+++ src/main/java/com/e451/rest/security/JwtTokenUtil.java
@@ -84,7 +84,7 @@ public class JwtTokenUtil implements Serializable {
         try {
             JwtParser parser = Jwts.parser();
 
-            if(privateKeyEncoded == null || publicKeyEncoded == null) {
+            if(privateKeyEncoded.isEmpty() || publicKeyEncoded.isEmpty()) {
                 parser.setSigningKey("secret");
             } else {
                 parser.setSigningKey(getPublicKey());
@@ -124,7 +124,7 @@ public class JwtTokenUtil implements Serializable {
                     .setClaims(claims)
                     .setExpiration(generateExpirationDate());
 
-            if(privateKeyEncoded == null || publicKeyEncoded == null) {
+            if(privateKeyEncoded.isEmpty() || publicKeyEncoded.isEmpty()) {
                 builder.signWith(SignatureAlgorithm.HS256, "secret");
             } else {
                 builder.signWith(SignatureAlgorithm.RS256, getPrivateKey());
